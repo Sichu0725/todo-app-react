f72aa973554c06098dd9b17d4f0f6aab004b6da7
Sichu0725 <choihc8511@gmail.com>
2022-05-26 12:12:09 +0900

    오류를 심어놨다 ㅎㅎ

--- src/components/TodoLIst.js
+++ src/components/TodoLIst.js
@@ -1,21 +1,48 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import TodoListItem from './TodoListItem';
 import './TodoList.scss';
+import { List } from 'react-virtualized';
 const TodoList = ({ todos, onRemove, onToggle }) => {
+  const rowRender = useCallback(
+    ({ index, key, style }) => {
+      const todo = todos[index];
+      return (
+        <TodoListItem
+          todo={todo}
+          key={key}
+          onRemove={onRemove}
+          onToggle={onToggle}
+          style={style}
+        />
+      );
+    },
+    [onRemove, onToggle, todos],
+  );
+
   return (
-    <div className="TodoList">
-      {todos.map((todo) => {
-        return (
-          <TodoListItem
-            todo={todo}
-            onToggle={onToggle}
-            key={todo.id}
-            onRemove={onRemove}
-          />
-        );
-      })}
-    </div>
+    <List
+      className="TodoList"
+      width={512}
+      height={513}
+      rowCount={todos.length}
+      rowHeight={57}
+      rowRenderer={rowRender}
+      list={todos}
+      style={{ outline: 'none' }}
+    />
   );
 };
+// <div className="TodoList">
+//   {todos.map((todo) => {
+//     return (
+//       <TodoListItem
+//         todo={todo}
+//         onToggle={onToggle}
+//         key={todo.id}
+//         onRemove={onRemove}
+//       />
+//     );
+//   })}
+// </div>
 
 export default React.memo(TodoList);
--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -7,23 +7,31 @@ import {
   MdRemoveCircleOutline,
 } from 'react-icons/md';
 
-const TodoListItem = ({ todo, onRemove, onToggle }) => {
+const TodoListItem = ({ todo, onRemove, onToggle, style }) => {
   const { id, text, checked } = todo;
 
   const onClick = useCallback(() => {
     onRemove(id);
   }, [onRemove, id]);
   return (
-    <div className="TodoListItem">
-      <div className={cn('checkbox', { checked })} onClick={() => onToggle(id)}>
-        {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
-        <div className="text">{text}</div>
-      </div>
-      <div className="remove" onClick={onClick}>
-        <MdRemoveCircleOutline />
+    <div className="TodoListItem-virtualized" style={style}>
+      <div className="TodoListItem">
+        <div
+          className={cn('checkbox', { checked })}
+          onClick={() => onToggle(id)}
+        >
+          {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+          <div className="text">{text}</div>
+        </div>
+        <div className="remove" onClick={onClick}>
+          <MdRemoveCircleOutline />
+        </div>
       </div>
     </div>
   );
 };
 
-export default React.memo(TodoListItem);
+export default React.memo(
+  TodoListItem,
+  (prevProps, nextProps) => prevProps.todo === nextProps.todo,
+);
